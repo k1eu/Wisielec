6b174504b8ce39a85b31720b3a2824b13b41b33b
tkielarbb <tomek.kielar00@gmail.com>
2019-04-22 09:36:20 +0200

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Tomasz Kielar on 22/04/2019.
+//  Copyright © 2019 Tomasz Kielar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
